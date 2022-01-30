55c3244f1a87afb609f04c4b707f4b53c65c33d6
QI-Joe <80494923+QI-Joe@users.noreply.github.com>
2022-01-30 16:17:14 +0800

    new function for decode tx input

--- Abi_checker.py
+++ Abi_checker.py
@@ -1,23 +1,18 @@
-from ctypes import cast
-from time import time
-from eth_typing import HexStr
-from hexbytes import HexBytes
+import hexbytes
 from web3 import Web3
-import pre_read  
-import sys
-sys.path.append("/python3.10/Lib/site-packages/web3/_utils")
-import abi  # can be accessed and used
-sys.path.append("/python3.10/Lib/site-packages/eth_abi")
-import codec   # can be accessed but hard to be used
+import pre_read 
+import time
+import timeit
 
 class checker:
     w3 = None
-    my_key=None
     tx_hash=None
-    mapper = None
-    path = None
+    event = None
+    functions = None
+    ABI_file = None
+    file_name = None
     
-    def __init__(self, path, w3, my_key, tx_hash) -> None:
+    def __init__(self, path, w3, tx_hash) -> None:
         if w3 is None: 
             print("connection fails, please check it. program will stop in 3 seconds")
             time.sleep(3)
@@ -29,31 +24,56 @@ class checker:
             tx_hash = input("tx_hash: ")
         self.tx_hash=tx_hash
         
-        self.my_key=my_key
-        self.mapper = pre_read.reader().pre_map()
-        
+        self.event, self.functions, self.ABI_file, self.file_name = pre_read.reader(path).pre_map()
+        self.file_name = Web3.toChecksumAddress(self.file_name)
     
-    def get_tx(self) -> HexStr:
-        tx = self.w3.eth.get_transaction(self.tx_hash)['input']
-        return tx    
+    def get_tx(self):
+        tx = self.w3.eth.get_transaction(self.tx_hash)  # this step cost the longest time
+        receipt = self.w3.eth.get_transaction_receipt(self.tx_hash)
+        return tx, receipt
     
-    def parameters(self, variable, module) :
-        variable = HexBytes(variable)
-        types = abi.get_abi_input_types(module)
+    def decode_tx_info(self):
+        contract = self.w3.eth.contract(address = self.file_name, abi = self.ABI_file)  # according to its source code, this actually run in local
+        input, receipt = self.get_tx()
+        matched = None
         
-        decoded = codec.ABIDecoder.decode_abi(types, cast(HexBytes, variable))  # next stage need to be implemented
-        return decoded
+        if input['input'][0:67] in self.event:
+            matched = self.event[input[0:67]]
+            # special process of event, wont go into next stage  this place will be implented in next file
+            # do something to variable receipt
+            return matched  # do input must be function type ?
+        elif input['input'][0:10] in self.functions:
+            matched = self.functions[input['input'][0:10]]
+            
+        if matched == None: 
+            print("input cannot match with current ABI files")
+            return False
+        
+        for data in input:
+            if data == 'input':
+                input_result = contract.decode_function_input(input[data])
+                for i in range(len(input_result[1])):
+                    matched['inputs'][i]["output"] = input_result[1][matched['inputs'][i]['name']]
+                matched['function full name'] = input_result[0]
+            else: 
+                if type(input[data]) == hexbytes.main.HexBytes:
+                    matched[data] = input[data].hex()
+                else: matched[data] = input[data]
+        # print(matched)
+        return matched
     
-    def parse_rawtx(self) -> dict:
-        tx= self.get_tx()
-        signatures = tx[0:10]
-        parameters = tx[10 : ]
-        module = self.mapper[signatures]
-        self.mapper[signatures] = self.parameters(parameters, module)
-        return self.mapper
-        
-w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/Your Project"))
-path = input("input your json file path here: ")   #or you can skip this step and directly set your path
-jack = checker( path, w3, "Your Ethereum Key", "0x854ee8eef97da539b67cd7c9dd09c408688aa1de26d38f4dc25aaa7d052c8cc6")
-# 0x854ee8eef97da539b67cd7c9dd09c408688aa1de26d38f4dc25aaa7d052c8cc6 is a transaction hash
-print(jack.parse_rawtx())
+    
+# for the rest of codes you can change them or test it by youself
+def test():    
+    w3 = Web3(Web3.HTTPProvider("https://mainnet.infura.io/v3/Your Project"))
+    path = input("input your json file path here: ")   #or you can skip this step and directly set your path
+    start = timeit.default_timer()
+    
+    jack = checker(path, w3, "0x854ee8eef97da539b67cd7c9dd09c408688aa1de26d38f4dc25aaa7d052c8cc6")
+    # 0x854ee8eef97da539b67cd7c9dd09c408688aa1de26d38f4dc25aaa7d052c8cc6 is a transaction hash
+    
+    print(jack.decode_tx_info())
+    stop = timeit.default_timer()
+    print("Times is: " + str(stop - start))  # this one is used to test its running time. On my computer it is about 1.2s
+    
+test()
--- pre_read.py
+++ pre_read.py
@@ -1,3 +1,5 @@
+from importlib.resources import path
+import ntpath
 import ujson as json
 
 class reader:
@@ -5,7 +7,6 @@ class reader:
     
     def __init__(self, path) -> None:
         self.path = path
-        pass 
     
     def my_read_json(self):
         with open (self.path, 'r') as fp:                        #, default = lambda  obj:obj.__dict__, sort_keys=True, indent=4
@@ -14,9 +15,11 @@ class reader:
         fp.close()
         return files                                                                              # can we merge my_read_json and pre_map into one?
                                                                                                        # I mean, mapping signature with every parenthetical while reading the json file
-    def pre_map(self) -> dict:                                                          # so this only need Theta(n) times instead of scanning whole file twice
+    def pre_map(self):                                                          # so this only need Theta(n) times instead of scanning whole file twice
         file = self.my_read_json()
-        stores = dict()
+        event, functions = dict(), dict()
+        contract_address = ntpath.basename(self.path)[:-5]
         for parenthetical in file:
-            stores[parenthetical["signature"]] = parenthetical
-        return stores
+            if (parenthetical['type'] == 'event'): event[parenthetical['signature']] = parenthetical
+            elif (parenthetical['type'] == 'function'): functions[parenthetical["signature"]] = parenthetical
+        return event, functions, file, contract_address
